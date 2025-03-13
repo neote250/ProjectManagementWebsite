3a9a0cad8a7c1d6c4a43a6af8261a5097d03f8a2
BruceWayne52411 <158220789+BruceWayne52411@users.noreply.github.com>
2025-03-13 09:49:34 -0600

    v5

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'public', // Set the output directory to 'public' for Vercel
+    outDir: 'public, build', // Set the output directory to 'public' for Vercel
   },
 });
