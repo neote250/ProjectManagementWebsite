4f20f4a9b3ed2fbd24d767275165b8dce3a38b57
BruceWayne52411 <158220789+BruceWayne52411@users.noreply.github.com>
2025-03-14 00:38:15 -0600

    nope

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
   }, []);
 
   const fetchUserData = async (token) => {
-    const response = await fetch("http://localhost:5000/api/user", {
+    const response = await fetch("https://projectmanagement-brucewayne52411s-projects.vercel.app/", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
